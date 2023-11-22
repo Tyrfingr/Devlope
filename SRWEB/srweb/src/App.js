ae89cfdee2cdf051a6f9e9e57040b1495b94d5cb
Tyrfingr <wnsgh587@gmail.com>
2023-11-22 18:09:22 +0900

    good

---
+++ SRWEB/srweb/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {ButtonAppBar} from './menubar.js';
+import * as React from 'react';
+import {IconMenu} from './menus.js';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar/>
+    </div>
+  );
+}
+
+export default App;
